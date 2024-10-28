1386b58a932d7dc6ad3fa801ad1277ac5571c42e
letmerecall <girishsharma001@gmail.com>
2024-10-28 13:48:11 +0530

    Update private ai source check

--- nemoguardrails/library/privateai/actions.py
+++ nemoguardrails/library/privateai/actions.py
@@ -40,11 +40,12 @@ async def detect_pii(source: str, text: str, config: RailsConfig):
 
     pai_config: PrivateAIDetection = getattr(config.rails.config, "privateai")
 
-    assert source in [
-        "input",
-        "output",
-        "retrieval",
-    ], f"Private AI can only be defined in the input, output and retrieval flows. The current flow {source} is not allowed."
+    valid_sources = ["input", "output", "retrieval"]
+    if source not in valid_sources:
+        raise ValueError(
+            f"Private AI can only be defined in the following flows: {valid_sources}. "
+            f"The current flow, '{source}', is not allowed."
+        )
 
     entity_detected = await private_ai_detection_request(
         text,
