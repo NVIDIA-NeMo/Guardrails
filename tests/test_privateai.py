da2f547afcba84b6664c378a57576c6fa74efb2f
letmerecall <girishsharma001@gmail.com>
2024-12-11 20:18:33 +0530

    Add tests for private ai pii masking

--- tests/test_privateai.py
+++ tests/test_privateai.py
@@ -37,6 +37,13 @@ def mock_detect_pii(return_value=True):
     return mock_request
 
 
+def mock_pii_masking(return_value):
+    def mock_request(*args, **kwargs):
+        return return_value
+
+    return mock_request
+
+
 @pytest.mark.unit
 def test_privateai_pii_detection_no_active_pii_detection():
     config = RailsConfig.from_content(
@@ -257,3 +264,173 @@ def test_privateai_pii_detection_retrieval_with_no_pii():
 
     chat >> "Hi!"
     chat << "Hi! My name is John as well."
+
+
+@pytest.mark.unit
+def test_privateai_pii_masking_on_output():
+    config = RailsConfig.from_content(
+        yaml_content="""
+            models: []
+            rails:
+              config:
+                privateai:
+                  server_endpoint: https://api.private-ai.com/cloud/v3/process/text
+                  output:
+                    entities:
+                      - EMAIL_ADDRESS
+                      - NAME
+              output:
+                flows:
+                  - mask pii on output
+        """,
+        colang_content="""
+            define user express greeting
+              "hi"
+
+            define flow
+              user express greeting
+              bot express greeting
+
+            define bot inform answer unknown
+              "I can't answer that."
+        """,
+    )
+
+    chat = TestChat(
+        config,
+        llm_completions=[
+            "  express greeting",
+            '  "Hi! I am John.',
+        ],
+    )
+
+    chat.app.register_action(retrieve_relevant_chunks, "retrieve_relevant_chunks")
+    chat.app.register_action(mock_pii_masking("Hi! I am [NAME_GIVEN_1]."), "mask_pii")
+
+    chat >> "Hi!"
+    chat << "Hi! I am [NAME_GIVEN_1]."
+
+
+@pytest.mark.unit
+def test_privateai_pii_masking_on_input():
+    config = RailsConfig.from_content(
+        yaml_content="""
+            models: []
+            rails:
+              config:
+                privateai:
+                  server_endpoint: https://api.private-ai.com/cloud/v3/process/text
+                  input:
+                    entities:
+                      - EMAIL_ADDRESS
+                      - NAME
+              input:
+                flows:
+                  - mask pii on input
+                  - check user message
+        """,
+        colang_content="""
+            define user express greeting
+              "hi"
+
+            define flow
+              user express greeting
+              bot express greeting
+
+            define bot inform answer unknown
+              "I can't answer that."
+
+            define flow check user message
+              execute check_user_message(user_message=$user_message)
+        """,
+    )
+
+    chat = TestChat(
+        config,
+        llm_completions=[
+            "  express greeting",
+            '  "Hi! I am John.',
+        ],
+    )
+
+    @action()
+    def check_user_message(user_message: str):
+        """Check if the user message is converted to the expected message with masked PII."""
+        assert user_message == "Hi there! Are you [NAME_1]?"
+
+    chat.app.register_action(retrieve_relevant_chunks, "retrieve_relevant_chunks")
+    chat.app.register_action(
+        mock_pii_masking("Hi there! Are you [NAME_1]?"), "mask_pii"
+    )
+    chat.app.register_action(check_user_message, "check_user_message")
+
+    chat >> "Hi there! Are you John?"
+    chat << "Hi! I am John."
+
+
+@pytest.mark.unit
+def test_privateai_pii_masking_on_retrieval():
+    config = RailsConfig.from_content(
+        yaml_content="""
+            models: []
+            rails:
+              config:
+                privateai:
+                  server_endpoint: https://api.private-ai.com/cloud/v3/process/text
+                  retrieval:
+                    entities:
+                      - EMAIL_ADDRESS
+                      - NAME
+              retrieval:
+                flows:
+                  - mask pii on retrieval
+                  - check relevant chunks
+        """,
+        colang_content="""
+            define user express greeting
+              "hi"
+
+            define flow
+              user express greeting
+              bot express greeting
+
+            define bot inform answer unknown
+              "I can't answer that."
+
+            define flow check relevant chunks
+              execute check_relevant_chunks(relevant_chunks=$relevant_chunks)
+        """,
+    )
+
+    chat = TestChat(
+        config,
+        llm_completions=[
+            "  express greeting",
+            "  Sorry, I don't have that in my knowledge base.",
+        ],
+    )
+
+    @action()
+    def check_relevant_chunks(relevant_chunks: str):
+        """Check if the relevant chunks is converted to the expected message with masked PII."""
+        assert relevant_chunks == "[NAME_1]'s Email: [EMAIL_ADDRESS_1]"
+
+    @action()
+    def retrieve_relevant_chunk_for_masking():
+        # Mock retrieval of relevant chunks with PII
+        context_updates = {"relevant_chunks": "John's Email: john@email.com"}
+        return ActionResult(
+            return_value=context_updates["relevant_chunks"],
+            context_updates=context_updates,
+        )
+
+    chat.app.register_action(
+        retrieve_relevant_chunk_for_masking, "retrieve_relevant_chunks"
+    )
+    chat.app.register_action(
+        mock_pii_masking("[NAME_1]'s Email: [EMAIL_ADDRESS_1]"), "mask_pii"
+    )
+    chat.app.register_action(check_relevant_chunks)
+
+    chat >> "Hey! Can you help me get John's email?"
+    chat << "Sorry, I don't have that in my knowledge base."
