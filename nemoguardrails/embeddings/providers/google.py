026a34489c8197aa5a21e415f5f8c1aa805e4653
kimbwook <bwook00@naver.com>
2025-07-24 14:01:34 +0900

    add google embedding provider

---
+++ nemoguardrails/embeddings/providers/google.py
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List
+
+from .base import EmbeddingModel
+
+
+class GoogleEmbeddingModel(EmbeddingModel):
+    """Embedding model using langchain-google-ai.
+
+    This class is a wrapper for using embedding models powered by Google AI (hosted in the Google Cloud).
+    To use this model, set `GOOGLE_API_KEY` in the environment variable.
+
+    Args:
+        embedding_model (str): The name of the embedding model to be used.
+
+    Attributes:
+        model: The name of the model to be called for creating embeddings.
+    """
+
+    engine_name = "google"
+
+    def __init__(self, embedding_model: str, **kwargs):
+        try:
+            from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+            self.model = embedding_model
+            self.document_embedder = GoogleGenerativeAIEmbeddings(
+                model=embedding_model, **kwargs
+            )
+
+        except ImportError:
+            raise ImportError(
+                "Could not import langchain_google_genai, please install it with "
+                "`pip install langchain-google-genai`."
+            )
+
+    async def encode_async(self, documents: List[str]) -> List[List[float]]:
+        """Encode a list of documents into their corresponding sentence embeddings.
+
+        Args:
+            documents (List[str]): The list of documents to be encoded.
+
+        Returns:
+            List[List[float]]: The list of sentence embeddings, where each embedding is a list of floats.
+        """
+
+        result = await self.document_embedder.aembed_documents(documents)
+        return result
+
+    def encode(self, documents: List[str]) -> List[List[float]]:
+        """Encode a list of documents into their corresponding sentence embeddings.
+
+        Args:
+            documents (List[str]): The list of documents to be encoded.
+
+        Returns:
+            List[List[float]]: The list of sentence embeddings, where each embedding is a list of floats.
+        """
+        return self.document_embedder.embed_documents(documents)
