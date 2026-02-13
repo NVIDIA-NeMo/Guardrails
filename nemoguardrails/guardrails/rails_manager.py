27d87c3b252670ca0d514e2841e814283fca4481
tgasser-nv <200644301+tgasser-nv@users.noreply.github.com>
2026-02-13 16:44:32 -0600

    Remove todos (will do in future PR), fix docstring for _last_content_by_role, remove unused guardrails_models.py

--- nemoguardrails/guardrails/rails_manager.py
+++ nemoguardrails/guardrails/rails_manager.py
@@ -70,7 +70,6 @@ class RailsManager:
         if not self.input_flows:
             return RailResult(is_safe=True)
 
-        # TODO: Running these sequentially now, need to update to parallel and early-out if any rail fails
         for flow in self.input_flows:
             result = await self._run_input_rail(flow, messages)
             if not result.is_safe:
@@ -91,7 +90,6 @@ class RailsManager:
         if not self.output_flows:
             return RailResult(is_safe=True)
 
-        # TODO: Running these sequentially now, need to update to parallel and early-out if any rail fails
         for flow in self.output_flows:
             result = await self._run_output_rail(flow, messages, response)
             if not result.is_safe:
