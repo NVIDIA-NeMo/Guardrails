8810aa5558559b9d2368d4fb8b1362ab28c930c6
Aditya Singh <adisin650@gmail.com>
2026-05-06 00:45:52 -0700

    chore(kb): format cache key imports

--- nemoguardrails/kb/kb.py
+++ nemoguardrails/kb/kb.py
@@ -13,9 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 import logging
 import os
-import json
 from time import time
 from typing import Callable, List, Optional, cast
 
